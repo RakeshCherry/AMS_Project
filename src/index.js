f72a058922013fb72cd1d0b8f9f7f620713edab2
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-04-03 19:22:26 +0530

    added user and admin components

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-import { HashRouter } from 'react-router-dom';
-
-
-
+import { BrowserRouter} from 'react-router-dom';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
-// reportWebVitals();
-
-
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render((
-   <HashRouter basename={process.env.frontend}>
-     <App />
-   </HashRouter>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
 reportWebVitals();
